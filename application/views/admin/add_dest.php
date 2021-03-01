6dbd9d891c1fb1d0d8d90f1e850d671620233bf1
KaranSingh4183 <78732157+KaranSingh4183@users.noreply.github.com>
2021-03-01 14:17:12 +0530

    Update add_dest.php

--- application/views/admin/add_dest.php
+++ application/views/admin/add_dest.php
@@ -15,7 +15,7 @@
         <!-- ############ Content START-->
         <div id="content" class="flex">
             <!-- ############ Main START-->
-            <div class="card-header" style="text-align: center; text-transform: uppercase; font-size: 26px;margin-top: 20px;"><strong>Add Destination</strong></div>
+            <div class="card-header" style="text-align: center; text-transform: uppercase; font-size: 26px;margin-top: 20px;"><strong>Add GALLERY</strong></div>
             <div class="col-md-6 container" style="margin-top: 30px;">
                                 <div class="card">
                                     
