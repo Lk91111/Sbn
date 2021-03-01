4f2146fdffd58f9fe2258faefa59d5b931764c29
KaranSingh4183 <78732157+KaranSingh4183@users.noreply.github.com>
2021-03-01 15:24:47 +0530

    Update blog.php

--- application/views/user/blog.php
+++ application/views/user/blog.php
@@ -31,7 +31,7 @@
                                 <a href="post.html" class="blog-img"><img src="<?=  base_url()?>assets/images/gallery/as.jpg" class="img-fluid" alt=""></a>
                                 <div class="desc"> <span>Sep 2, 2020 | Cartoon editing</span>
                                     <h3><a href="post.html">Demon Slayer</a></h3>
-                                    <p>Fusce suscipit, ante a hendrerit ullamcorper, risus nisl cursus purus, sit amet viverra ante nulla vel justo. Morbi justo erat, posuere vel libero bibendum convallis enim.</p>
+                                    <p>A family is attacked by demons and only two members survive - Tanjiro and his sister Nezuko, who is turning into a demon slowly. Tanjiro sets out to become a demon slayer to avenge his family and cure his sister. ... Demon Slayer focuses on Tanjirou Kamado, who is still very young, but is the only man in his family.</p>
                                 </div>
                             </div>
                         </div>
@@ -40,7 +40,7 @@
                                 <a href="post.html" class="blog-img"><img src="<?=  base_url()?>assets/images/gallery/ae.jpg" class="img-fluid" alt=""></a>
                                 <div class="desc"> <span>Sep 13, 2020 | Anime editing</span>
                                     <h3><a href="post.html">Attack On Titan</a></h3>
-                                    <p>Fusce suscipit, ante a hendrerit ullamcorper, risus nisl cursus purus, sit amet viverra ante nulla vel justo. Morbi justo erat, posuere vel libero bibendum convallis enim.</p>
+                                    <p>The plot of Attack on Titan centers on a civilization inside three walls, the last location where humans still live. Over one hundred years ago, humanity was driven to the brink of extinction after the emergence of humanoid giants called Titans, who attack and eat humans on sight.</p>
                                 </div>
                             </div>
                         </div>
@@ -49,7 +49,7 @@
                                 <a href="post.html" class="blog-img"><img src="<?=  base_url()?>assets/images/gallery/ac.jpg" class="img-fluid" alt=""></a>
                                 <div class="desc"> <span>Sep 23, 2020 | Animated</span>
                                     <h3><a href="post.html">Your name</a></h3>
-                                    <p>Fusce suscipit, ante a hendrerit ullamcorper, risus nisl cursus purus, sit amet viverra ante nulla vel justo. Morbi justo erat, posuere vel libero bibendum convallis enim.</p>
+                                    <p>Two strangers find themselves linked in a bizarre way. When a connection forms, will distance be the only thing to keep them apart</p>
                                 </div>
                             </div>
                         </div>
@@ -58,7 +58,7 @@
                                 <a href="post.html" class="blog-img"><img src="<?=  base_url()?>assets/images/gallery/ab.jpg" class="img-fluid" alt=""></a>
                                 <div class="desc"> <span>Sep 28, 2020 | Paradox editing</span>
                                     <h3><a href="post.html">DARK</a></h3>
-                                    <p>Fusce suscipit, ante a hendrerit ullamcorper, risus nisl cursus purus, sit amet viverra ante nulla vel justo. Morbi justo erat, posuere vel libero bibendum convallis enim.</p>
+                                    <p> They follow connections between four estranged families to unravel a sinister time travel conspiracy which spans several generations</p>
                                 </div>
                             </div>
                         </div>
