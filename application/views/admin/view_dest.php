92121312c16d7bd68037fd3fedcae22ab5647666
KaranSingh4183 <78732157+KaranSingh4183@users.noreply.github.com>
2021-03-01 14:17:20 +0530

    Update view_dest.php

--- application/views/admin/view_dest.php
+++ application/views/admin/view_dest.php
@@ -15,7 +15,7 @@
         <!-- ############ Content START-->
         <div id="content" class="flex">
             <!-- ############ Main START-->
-            <div class="card-header" style="text-align: center; text-transform: uppercase; font-size: 26px;margin-top: 20px;"><strong>Destination Details</strong></div>
+            <div class="card-header" style="text-align: center; text-transform: uppercase; font-size: 26px;margin-top: 20px;"><strong>GALLERY Details</strong></div>
             <div class="col-md-12 container" style="margin-top: 30px;">
                 
                     <div class="padding">
